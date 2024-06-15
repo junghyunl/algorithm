ec3310fb96c8f509770dd4a91de16269aa5ec33a
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-06-16 02:27:09 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 288 ms, Memory: 43260 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
@@ -4,11 +4,11 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 let campus = input.slice(1).map(line => line.split(''));
 
-let queue = [];
+let stack = [];
 loop: for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
         if (campus[i][j] === 'I') {
-            queue.push([i, j]);
+            stack.push([i, j]);
             campus[i][j] = 'X';
             break loop;
         }
@@ -19,8 +19,8 @@ const dx = [-1,1,0,0];
 const dy = [0,0,-1,1];
 let result = 0;
 
-while (queue.length > 0) {
-    const [x, y] = queue.pop();
+while (stack.length > 0) {
+    const [x, y] = stack.pop();
 
     for (let i = 0; i < 4; i++) {
         const nx = x + dx[i];
@@ -29,7 +29,7 @@ while (queue.length > 0) {
         if (nx < 0 || nx >= n || ny < 0 || ny >= m || campus[nx][ny] === 'X') continue;
         if (campus[nx][ny] === 'P') result++;
         campus[nx][ny] = 'X';
-        queue.push([nx, ny]);
+        stack.push([nx, ny]);
     }
 }
 
