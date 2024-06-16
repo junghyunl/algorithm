3d9f8ab768d2c917f60d8dfd7d1a066405582ed2
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-06-17 01:17:26 +0900

    [Silver I] Title: IOIOI, Time: 424 ms, Memory: 16136 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -2,12 +2,19 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 //  /dev/stdin
 const [n, m] = [+input[0], +input[1]];
-const [Pn, Pn_1] = ['IO'.repeat(n)+'I', 'IO'.repeat(n-1)+'I'];
+const Pn = 'IO'.repeat(n)+'I';
 let s = input[2].trim();
 
 let result = 0;
-while (s.replace(Pn, Pn_1) !== s) {
-    s = s.replace(Pn, Pn_1);
-    result++;
+for (let i = 0; i < m+2*n; i++) {
+    if (s[i] === 'O') continue;
+    if (s.slice(i,i+2*n+1) === Pn) {
+        result++;
+        while (s[i+2*n+1] === 'O' && s[i+2*n+2] === 'I') {
+            result++;
+            i+=2;
+        }
+        i+=2*n;
+    }
 }
 console.log(result);
