5da32778c8bd15a978f57beaf3f3aa12f53e0f41
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-06-18 22:30:03 +0900

    [Gold V] Title: 토마토, Time: 696 ms, Memory: 147040 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+//  /dev/stdin
+const [m, n, h] = input[0].split(' ').map(Number);
+let tomato = [];
+for (let i = 0; i < h; i++) {
+    tomato.push(input.slice(i*n+1, (i+1)*n+1).map(line => line.split(' ').map(Number)));
+}
+
+let queue = [];
+let unripe = 0;
+for (let i = 0; i < h; i++) {
+    for (let j = 0; j < n; j++) {
+        for (let k = 0; k < m; k++) {
+            if (tomato[i][j][k] === 0) unripe++;
+            else if (tomato[i][j][k] === 1) queue.push([i,j,k]);
+        }
+    }
+}
+
+const dx = [-1,1,0,0,0,0];
+const dy = [0,0,-1,1,0,0];
+const dz = [0,0,0,0,-1,1];
+
+let idx = 0;
+let maxValue = 0;
+while (queue.length > idx) {
+    const [z, x, y] = queue[idx++];
+
+    for (let i = 0; i < 6; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+        const nz = z + dz[i];
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
+        if (tomato[nz][nx][ny] === 0) {
+            queue.push([nz,nx,ny]);
+            tomato[nz][nx][ny] = tomato[z][x][y] + 1;
+            maxValue = Math.max(maxValue, tomato[z][x][y]);
+            unripe--;
+        }
+    }
+}
+
+console.log(unripe ? -1 : maxValue);
