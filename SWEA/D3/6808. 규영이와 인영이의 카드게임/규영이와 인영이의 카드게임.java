f7761a0191a37715fc080921d1b0fcd41de9cc8e
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-13 16:56:56 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,795 ms, Memory: 30,388 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,101 +1,67 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
+
 import java.util.Scanner;
 import java.util.Arrays;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
 class Solution
 {
 	public static int[] player1;
-	public static int[] player2;
-	public static final int N = 9;
-	public static int[] cards;
-	public static boolean[] used;
-	public static int winCount;
-	public static int loseCount;
-	
-	public static void whoWin(int[] player2Cards) {
-		int kScore = 0;
-		int iScore = 0;
-		for (int i = 0; i < 9; i++) {
-			if (player1[i] > player2Cards[i]) kScore += player1[i] + player2Cards[i];
-			else if (player1[i] < player2Cards[i]) iScore += player1[i] + player2Cards[i];
-		}
-		if (iScore > kScore) loseCount++;
-		if (iScore < kScore) winCount++;
-	}
-	
-	public static void permutation(int depth) {
-		if (depth == N) {
-			whoWin(cards);
-			return;
-		}
-		for (int i = 0; i < 9; i++) {
-			if (used[i]) continue;
-			used[i] = true;
-			cards[depth] = player2[i];
-			permutation(depth + 1);
-			used[i] = false;
-		}
-	}
-	
-	public static void main(String args[]) throws Exception
-	{
-
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			player1 = new int[N];
-			player2 = new int[N];
-			cards = new int[N];
-			used = new boolean[N];
-			winCount = 0;
-			loseCount = 0;
-			for (int i = 0; i < 9; i++) {
-				player1[i] = sc.nextInt();
-			}
-			int j = 0;
-			for (int i = 1; i <= 18; i++) {
-				int num = i;
-				if (!Arrays.stream(player1).anyMatch(x -> x == num)) player2[j++] = i;
-			}
-			
-			
-			permutation(0);
-			
-			System.out.println("#" + test_case + " " + winCount + " " + loseCount);
-
-		}
-	}
+    public static int[] player2;
+    public static final int N = 9;
+    public static int[] cards;
+    public static int winCount;
+    public static int loseCount;
+     
+    public static void whoWin(int[] player2Cards) {
+        int kScore = 0;
+        int iScore = 0;
+        for (int i = 0; i < 9; i++) {
+            if (player1[i] > player2Cards[i]) kScore += player1[i] + player2Cards[i];
+            else if (player1[i] < player2Cards[i]) iScore += player1[i] + player2Cards[i];
+        }
+        if (iScore > kScore) loseCount++;
+        if (iScore < kScore) winCount++;
+    }
+     
+    public static void permutation(int depth, int flag) {
+        if (depth == N) {
+            whoWin(cards);
+            return;
+        }
+        for (int i = 0; i < 9; i++) {
+            if ((flag & 1 << i) != 0) continue;
+            cards[depth] = player2[i];
+            permutation(depth + 1, flag | 1 << i);
+        }
+    }
+     
+    public static void main(String args[]) throws Exception
+    {
+ 
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            player1 = new int[N];
+            player2 = new int[N];
+            cards = new int[N];
+            winCount = 0;
+            loseCount = 0;
+            for (int i = 0; i < 9; i++) {
+                player1[i] = sc.nextInt();
+            }
+            int j = 0;
+            for (int i = 1; i <= 18; i++) {
+                int num = i;
+                if (!Arrays.stream(player1).anyMatch(x -> x == num)) player2[j++] = i;
+            }
+             
+             
+            permutation(0, 0);
+             
+            System.out.println("#" + test_case + " " + winCount + " " + loseCount);
+ 
+        }
+    }
 }
