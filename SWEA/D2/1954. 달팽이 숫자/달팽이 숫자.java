98a9bbadd3e41203edddf201fefdc3c538b08fa1
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-13 16:21:26 +0900

    [D2] Title: 달팽이 숫자, Time: 161 ms, Memory: 21,704 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static int N;
+	public static int[][] snail;
+	public static final int[] dx = {0,1,0,-1};
+	public static final int[] dy = {1,0,-1,0};
+	
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			N = sc.nextInt();
+			snail = new int[N][N];
+			int num = 1;
+			int idx = 0;
+			int x = 0;
+			int y = -1;
+			
+			while (num <= N*N) {
+				if (x + dx[idx] < 0 || x + dx[idx] >= N || y + dy[idx] < 0 || y + dy[idx] >= N || snail[x + dx[idx]][y + dy[idx]] > 0) {
+					idx = (idx+1)%4;
+					continue;
+				}
+				x += dx[idx];
+				y += dy[idx];
+				
+				snail[x][y] = num++;
+			}
+			
+			System.out.println("#" + test_case);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(snail[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+		}
+	}
+}
