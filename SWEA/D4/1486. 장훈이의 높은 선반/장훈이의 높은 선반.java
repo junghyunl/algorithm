2641bdb7f9c075c96fb754843c6ed6eeb58d231d
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-22 13:19:11 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 110 ms, Memory: 21,040 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,21 +1,21 @@
-import java.io.*;
 import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-	static int T, N, minH, res;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int[] heights;
-	
-	public static void main(String[] args) throws IOException{
-		T = Integer.parseInt(br.readLine());
+     static int N, B, answer;
+     static int[] heights;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		for(int tc=1; tc<=T; tc++) {
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			minH = Integer.parseInt(st.nextToken());
-			res = 200000;
-			
+			B = Integer.parseInt(st.nextToken());
+			answer = Integer.MAX_VALUE;
 			heights = new int[N];
 			
 			st = new StringTokenizer(br.readLine());
@@ -23,20 +23,18 @@ public class Solution {
 				heights[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			subset(0, 0);
-			System.out.println("#" + tc + " " + (res-minH));
+			subSet(0,0);
+			System.out.println("#" + tc + " " + (answer-B));
 		}
-	}
-	
-	public static void subset(int depth, int sum) {
-		if(depth==N) {
-			if(sum<minH) return;
-			if(sum<res) res = sum;
+    }
+    
+    public static void subSet(int depth, int total) {
+		if (depth == N) {
+			if (total < B) return;
+			answer = Math.min(answer, total);
 			return;
 		}
-		
-		subset(depth+1, sum); // 비선택
-		subset(depth+1, sum+heights[depth]); // 선택
+		subSet(depth+1, total+heights[depth]);
+		subSet(depth+1, total);
 	}
-
 }
