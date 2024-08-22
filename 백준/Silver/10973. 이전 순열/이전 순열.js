f4f00b865c01802e88c426d92a80a6ec3cfcfff3
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-22 10:13:47 +0900

    [Silver III] Title: 이전 순열, Time: 104 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.js
@@ -0,0 +1,23 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
+
+const N = +input[0];
+let numbers = input[1].split(' ').map(Number);
+
+function findArr() {
+    for (let i = N-1; i > 0; i--) {
+        if (numbers[i - 1] > numbers[i]) {
+            let index = i;
+            for (let j = i; j < N; j++) {
+                if (numbers[j] > numbers[i - 1]) continue;
+                if (numbers[j] > numbers[index]) index = j;
+            }
+            [numbers[i - 1], numbers[index]] = [numbers[index], numbers[i - 1]];
+            numbers = numbers.slice(0, i).concat(numbers.slice(i).sort((a, b) => b-a));
+            return numbers.join(' ');
+        }
+    }
+    return -1;
+}
+
+console.log(findArr());
