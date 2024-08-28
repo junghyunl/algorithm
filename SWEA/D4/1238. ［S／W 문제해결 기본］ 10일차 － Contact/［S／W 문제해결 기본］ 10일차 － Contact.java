775a7f8feab99aad54781fe9c7b80394bbb503ca
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-28 23:11:06 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 131 ms, Memory: 21,096 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class Solution {
 	
 	static int N, start, maxDepth, ans;
-	static Node[] adjList;
+	static ArrayList<Integer>[] adjList;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,14 +18,17 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			start = Integer.parseInt(st.nextToken());
-			adjList = new Node[101];
+			adjList = new ArrayList[101];
 			maxDepth = 0; ans = 0;
+			for (int i = 1; i < 101; i++) {
+				adjList[i] = new ArrayList<>();
+			}
 			
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N/2; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				adjList[from] = new Node(to, adjList[from]);
+				adjList[from].add(to);
 			}
 			bfs();
 			System.out.println("#" + tc + " " + ans);
@@ -43,24 +46,14 @@ public class Solution {
 			if (c.depth > maxDepth) {
 				maxDepth = c.depth;
 				ans = c.v;
-			}else if (c.depth == maxDepth){
-				ans = Math.max(ans, c.v);
-			}
+			}else if (c.depth == maxDepth) ans = Math.max(ans, c.v);
 			
-			for (Node temp = adjList[c.v]; temp != null; temp = temp.next) {
-				if(!visited[temp.v]) {
-					queue.offer(new Cur(temp.v, c.depth+1));
-					visited[temp.v] = true;
+			for (int temp : adjList[c.v]) {
+				if(!visited[temp]) {
+					queue.offer(new Cur(temp, c.depth+1));
+					visited[temp] = true;
 				}
 			}
-		}
-	}
-	static class Node {
-		int v;
-		Node next;
-		public Node(int v, Node next) {
-			this.v = v;
-			this.next = next;
 		}
 	}
 	static class Cur {
