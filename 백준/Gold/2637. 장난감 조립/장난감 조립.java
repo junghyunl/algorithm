0ea7cb3c87168e5917c4b2844c1aeac2c0c09171
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-08-31 15:07:17 +0900

    [Gold II] Title: 장난감 조립, Time: 124 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.java
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.java
@@ -3,54 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int N, M;
-	static int[] inDegree;
+	static int N;
 	static int[][] cnt;
-	static boolean[] checked;
-	static ArrayList<Integer>[] up, down;
-	static ArrayList<Integer> base;
+	static List<Integer> base;
 	static Queue<Integer> q;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder("");
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		cnt = new int[N+1][N+1];
-		checked = new boolean[N+1];
 		
-		up = new ArrayList[N+1];	
-		down = new ArrayList[N+1];
-		inDegree = new int[N+1];
-		q = new LinkedList<>();
+		N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		cnt = new int[N+1][N+1];
+		base = new ArrayList<>();
+		List<Edge>[] edges = new ArrayList[N+1];
+		int[] degree = new int[N+1];
 		
 		for (int i = 1; i < N+1; i++) {
-			up[i] = new ArrayList<>();
-			down[i] = new ArrayList<>();
+			edges[i] = new ArrayList<>();
 		}
-		base = new ArrayList<>();
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			cnt[from][to] = Integer.parseInt(st.nextToken());
-			down[from].add(to);
-			up[to].add(from);
-			inDegree[from]++;
+			int from = Integer.parseInt(st.nextToken());
+			
+			edges[from].add(new Edge(to, Integer.parseInt(st.nextToken())));
+			degree[to]++;
 		}
+		
+		q = new LinkedList<>();
 		for (int i = 1; i < N+1; i++) {
-			if(inDegree[i] == 0) {
-				for (int ad : up[i]) {
-					q.offer(ad);
-				}
-				checked[i] = true;
+			if(degree[i] == 0) {
+				q.offer(i);
 				cnt[i][i] = 1;
 				base.add(i);
 			}
@@ -59,35 +51,29 @@ public class Main {
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			
-			if (checked[cur]) continue;
-			if (!check(cur)) q.add(cur);
-			else {
-				checked[cur] = true;
-				baseCount(cur);
-				for (int ad : up[cur]) {
-					q.offer(ad);
-				}
+			for (Edge e : edges[cur]) {
+				baseCount(cur, e);
+				if (--degree[e.v] == 0) q.offer(e.v);
 			}
 		}
 			
+		StringBuilder ans = new StringBuilder();
 		for (int b : base) {
-			sb.append(b + " " + cnt[N][b] + "\n");
+			ans.append(b + " " + cnt[N][b] + "\n");
 		}
-		System.out.println(sb);
+		System.out.println(ans);
 	}
-	static void baseCount(int cur) {
-		for (int ad : down[cur]) {
-			if (base.contains(ad)) continue;
-			for (int b : base) {
-				cnt[cur][b] += cnt[cur][ad]*cnt[ad][b];
-			}
-			cnt[cur][ad] = 0;
+	static void baseCount(int from, Edge e) {
+		for (int b : base) {
+			cnt[e.v][b] += e.weight*cnt[from][b];
 		}
-	}
-	static boolean check(int cur) { // 체크된 부품인지
-		for (int ad : down[cur]) {
-			if (!checked[ad]) return false;
+	}	
+	static class Edge {
+		int v, weight;
+
+		public Edge(int v, int weight) {
+			this.v = v;
+			this.weight = weight;
 		}
-		return true;
 	}
 }
