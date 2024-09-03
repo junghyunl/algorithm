bedd13a0ebfe4092efd2b43ed458a19dbb4aeba6
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-03 19:47:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 133 ms, Memory: 20,596 KB -BaekjoonHub

---
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int N, M, ans;
+	static int[][][] cnt;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			cnt = new int[N][N][N+1];
+			ans = 0;
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					if (Integer.parseInt(st.nextToken()) == 1) check(i,j);
+				}
+			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (cnt[i][j][0] > ans && cnt[i][j][0]*M >= 1) ans = cnt[i][j][0];
+					for (int k = 1; k < N+1; k++) {
+						cnt[i][j][k] += cnt[i][j][k-1];
+						if (cnt[i][j][k] > ans && cnt[i][j][k]*M >= k*k+(k+1)*(k+1)) ans = cnt[i][j][k];
+					}
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+	static void check(int r, int c) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (Math.abs(i-r)+Math.abs(j-c) <= N) cnt[i][j][Math.abs(i-r)+Math.abs(j-c)]++;
+			}
+		}
+	}
+}
