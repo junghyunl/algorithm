12cf7849e8ff84468004215600fea8e85030206b
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-04 15:53:19 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -11,16 +11,16 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int[][] cost = new int[N+1][3];
 		
-		for (int i = 1; i < N+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			cost[i][0] = Integer.parseInt(st.nextToken());
-			cost[i][1] = Integer.parseInt(st.nextToken());
-			cost[i][2] = Integer.parseInt(st.nextToken());
-		}
+		st = new StringTokenizer(br.readLine());
+		cost[1][0] = Integer.parseInt(st.nextToken());
+		cost[1][1] = Integer.parseInt(st.nextToken());
+		cost[1][2] = Integer.parseInt(st.nextToken());
+		
 		for (int i = 2; i < N+1; i++) {
-			cost[i][0] += Math.min(cost[i-1][1], cost[i-1][2]);
-			cost[i][1] += Math.min(cost[i-1][0], cost[i-1][2]);
-			cost[i][2] += Math.min(cost[i-1][0], cost[i-1][1]);
+			st = new StringTokenizer(br.readLine());
+			cost[i][0] = Math.min(cost[i-1][1], cost[i-1][2]) + Integer.parseInt(st.nextToken());
+			cost[i][1] = Math.min(cost[i-1][0], cost[i-1][2]) + Integer.parseInt(st.nextToken());
+			cost[i][2] = Math.min(cost[i-1][0], cost[i-1][1]) + Integer.parseInt(st.nextToken());
 		}
 		
 		System.out.println(Math.min(Math.min(cost[N][0],cost[N][1]),cost[N][2]));
