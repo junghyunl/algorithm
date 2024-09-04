661ed709c5c1edee8b32dd9f36660ac2841f0646
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-04 16:04:31 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -11,12 +11,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int[][] cost = new int[N+1][3];
 		
-		st = new StringTokenizer(br.readLine());
-		cost[1][0] = Integer.parseInt(st.nextToken());
-		cost[1][1] = Integer.parseInt(st.nextToken());
-		cost[1][2] = Integer.parseInt(st.nextToken());
-		
-		for (int i = 2; i < N+1; i++) {
+		for (int i = 1; i < N+1; i++) {
 			st = new StringTokenizer(br.readLine());
 			cost[i][0] = Math.min(cost[i-1][1], cost[i-1][2]) + Integer.parseInt(st.nextToken());
 			cost[i][1] = Math.min(cost[i-1][0], cost[i-1][2]) + Integer.parseInt(st.nextToken());
