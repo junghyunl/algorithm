72ed3739efb2178160f43d75c3bc4d07e8f74e81
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-06 09:51:02 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,708 ms, Memory: 92,084 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int N, M, cnt, ans;
+	static boolean[][] adjMatrix;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			adjMatrix = new boolean[N][N];
+			ans = 0;
+			
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken())-1;
+				int to = Integer.parseInt(st.nextToken())-1;
+				adjMatrix[from][to] = true;
+			}
+
+			for (int i = 0; i < N; i++) {
+				boolean[] visited = new boolean[N];
+				cnt = 1;
+				downDfs(i, visited);
+				upDfs(i, visited);
+				if (cnt == N) ans++;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+	static void downDfs(int cur, boolean[] visited) {
+		visited[cur] = true;
+		
+		for (int i = 0; i < N; i++) {
+			if (adjMatrix[cur][i] && !visited[i]) {
+				downDfs(i, visited);
+				cnt++;
+			}
+		}
+	}
+	static void upDfs(int cur, boolean[] visited) {
+		visited[cur] = true;
+		
+		for (int i = 0; i < N; i++) {
+			if (adjMatrix[i][cur] && !visited[i]) {
+				upDfs(i, visited);
+				cnt++;
+			}
+		}
+	}
+}
