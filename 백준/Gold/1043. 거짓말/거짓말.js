aa55071dae466f821b00569f519e4741abd8a484
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-08 18:54:34 +0900

    [Gold IV] Title: 거짓말, Time: 92 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.js
+++ 백준/Gold/1043. 거짓말/거짓말.js
@@ -1,44 +1,42 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-//  /dev/stdin
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
-let knowTruth = new Array(n+1).fill(false);
-let relationship = Array.from({length:n+1}, () => []);
-const party = input.slice(2).map(a => a.split(' ').map(Number).slice(1));
 
-function countParty() {
-    if (+input[1] === 0) return m;
-
-    const stack = input[1].split(' ').map(Number).slice(1);
-
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < party[i].length-1; j++) {
-            for (let k = j+1; k < party[i].length; k++) {
-                relationship[party[i][j]].push(party[i][k]);
-                relationship[party[i][k]].push(party[i][j]);
-            }
-        }
-    }
-
-    while (stack.length > 0) {
-        const person = stack.pop();
-        if (knowTruth[person]) continue;
-        knowTruth[person] = true;
-
-        for (let i of relationship[person]) {
-            if (knowTruth[i]) continue;
-            stack.push(i);
-        }
-    }
-
-    let answer = 0;
-
-    for (let i = 0; i < m; i++) {
-        if (party[i].every(a => !knowTruth[a])) answer++;
-    }
-    
-    return answer;
+const list = input[1].split(' ').slice(1).map(a => +a-1);
+const truth = Array(n).fill(false);
+for (let i of list) {
+    truth[i] = true;
 }
 
-console.log(countParty());
+const parents = Array(n);
+for (let i = 0; i < n; i++) {
+    parents[i] = i;
+}
+
+const parties = input.slice(2).map(a => a.split(' ').map(a => +a-1));
+for (let i = 0; i < m; i++) {
+    for (let j = 1; j <= parties[i][0]; j++) {
+        union(parties[i][j], parties[i][j+1]);
+    }
+}
+
+let ans = 0;
+for (let i = 0; i < m; i++) {
+    if(!truth[find(parties[i][1])]) ans++;
+}
+
+console.log(ans);
+
+function find(a) {
+    if (parents[a] === a) return a;
+    return parents[a] = find(parents[a]);
+}
+
+function union(a, b) {
+    let [ar, br] = [find(a), find(b)];
+    if (ar === br) return false;
+    if (truth[ar]) [ar, br] = [br, ar];
+    parents[ar] = br;
+    return true;
+}
