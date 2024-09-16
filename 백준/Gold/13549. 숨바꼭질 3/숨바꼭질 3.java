f9b3c04e138b6a0f4f4401cc8918bb5f8d1c3d51
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-17 04:09:12 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 29200 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,51 +13,34 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int S = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		List<Node>[] adjList = new ArrayList[100001];
-		for (int i = 0; i < 100001; i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < 100000; i++) adjList[i].add(new Node(i+1, 1));
-		for (int i = 1; i < 100001; i++) adjList[i].add(new Node(i-1, 1));
-		for (int i = 1; i < 50001; i++) adjList[i].add(new Node(2*i, 0));
-		
-		System.out.println(dijkstra(adjList, S, E));
+		System.out.println(bfs(N, K));
 	}
-	static int dijkstra(List<Node>[] list, int start, int end) {
-		int[] minDistance = new int[100001];
-		Arrays.fill(minDistance, Integer.MAX_VALUE);
-		minDistance[start] = 0;
+	static int bfs(int n, int k) {
+		boolean[] visited = new boolean[100001];
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(n, 0));
 		
-		PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.weight - b.weight);
-		pq.offer(new Node(start, 0));
-		
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
+		while (q.peek().x != k) {
+			Node node = q.poll();
+			int nx = node.x;
+			int curCnt = node.cnt;
 			
-			if (cur.to == end) {
-				return cur.weight;
-			}
-			
-			if (cur.weight > minDistance[cur.to]) continue;
-			
-			for (Node node : list[cur.to]) {
-				if (minDistance[node.to] > cur.weight + node.weight) {
-					minDistance[node.to] = cur.weight + node.weight;
-					pq.offer(new Node(node.to, minDistance[node.to]));
-				}
-			}
+			if (nx < 0 || nx > 100000 || visited[nx]) continue;
+			visited[nx] = true;
+			q.offer(new Node(2*nx, curCnt));
+			q.offer(new Node(nx-1, curCnt+1));
+			q.offer(new Node(nx+1, curCnt+1));
 		}
-		return -1;
+		return q.poll().cnt;
 	}
 	static class Node {
-		int to, weight;
-		public Node(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
+		int x, cnt;
+		public Node(int x, int cnt) {
+			this.x = x;
+			this.cnt = cnt;
 		}
 	}
 }
