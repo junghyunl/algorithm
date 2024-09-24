0a67fb2f5846ce8f4391d679ddd3da7c39f87914
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-24 23:57:57 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.45 ms, Memory: 50 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,9 @@
+function solution(n, left, right) {
+    var answer = [];
+    
+    for (let i = left; i < right+1; i++) {
+        answer.push(Math.max(Math.floor(i/n), i%n)+1);
+    }
+    
+    return answer;
+}
