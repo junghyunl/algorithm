07ed30822c5ac4d41f5ff911aa3c452cee998258
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-25 02:04:55 +0900

    [Gold I] Title: 외판원 순회, Time: 268 ms, Memory: 33992 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.js
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.js
@@ -2,24 +2,21 @@ const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 const N = +input[0];
-const cost = input.slice(1).map(a => a.split(' ').map(a => +a === 0 ? Infinity : +a));
-const dp = Array.from({length:N}, () => Array.from({length:N-1}, () => Array(2**(N-1)).fill(Infinity)));
+const cost = input.slice(1).map(a => a.split(' ').map(Number));
+const dp = Array.from({length:N}, () => Array(1<<N).fill(-1));
 
-for (let i = 0; i < N-1; i++) {
-    dp[1][i][1<<i] = cost[N-1][i];
-}
-for (let i = 1; i < N-1; i++) {
-    for (let j = 0; j < N-1; j++) {
-        for (let k = 0; k < 2**(N-1); k++) {
-            if (dp[i][j][k] === Infinity) continue;
-            for (let l = 0; l < N-1; l++) {
-                dp[i+1][l][k|1<<l] = Math.min(dp[i+1][l][k|1<<l], dp[i][j][k]+cost[j][l]);
-            }
+function dfs(cur, visited) {
+    if (visited === (1<<N)-1) return cost[cur][0] || Infinity;
+    if (dp[cur][visited] > -1) return dp[cur][visited];
+
+    let res = Infinity;
+    for (let i = 1; i < N; i++) {
+        if ((visited & 1<<i) === 0 && cost[cur][i] > 0) {
+            res = Math.min(res, dfs(i, visited | 1<<i) + cost[cur][i]);
         }
     }
+    dp[cur][visited] = res;
+    return res;
 }
-let ans = Infinity;
-for (let i = 0; i < N-1; i++) {
-     ans = Math.min(ans, dp[N-1][i][2**(N-1)-1] + cost[i][N-1]);
-}
-console.log(ans);
+
+console.log(dfs(0, 1));
