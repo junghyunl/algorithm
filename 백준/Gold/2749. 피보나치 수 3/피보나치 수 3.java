c45f9b4ece07fc0c137dd15f0fc072f14cefeb86
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-09-26 09:56:23 +0900

    [Gold II] Title: 피보나치 수 3, Time: 132 ms, Memory: 26052 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		long[] dp = new long[1500000];
+		dp[1] = 1;
+		dp[2] = 1;
+		for (int i = 3; i < 1500000; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%1000000;
+		}
+		System.out.println(dp[(int)(N%1500000)]);
+	}
+}
