aa7ea3fc63212413e80d5950aeac8269e4cc79c7
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-10-02 03:40:58 +0900

    [Gold III] Title: 일감호에 다리 놓기, Time: 1200 ms, Memory: 353180 KB -BaekjoonHub

--- 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.java
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.java
@@ -7,45 +7,49 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		long K = Long.parseLong(st.nextToken());
 		
-		if (M < 2) System.out.println("YES");
-		else {
-			List<Node> list = new ArrayList<>();
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				list.add(new Node(i, Integer.parseInt(st.nextToken())));
-			}
-			Collections.sort(list, (a,b) -> Long.compare(a.weight, b.weight));
-			
-			boolean[][] check = new boolean[N][2];
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int j = Integer.parseInt(st.nextToken())-1;
-				int k = Integer.parseInt(st.nextToken())-1;
-				if ((j == 0 && k == N-1) || (k == 0 && j == N-1)) {
-					check[0][0] = true;
-					check[N-1][1] = true;
-				}else if (j < k){
-					check[k][0] = true;
-					check[j][1] = true;
-				}else {
-					check[j][0] = true;
-					check[k][1] = true;
-				}
-			}
-			long ans = mst(N, list, check);
-			System.out.println(ans > K ? "NO" : "YES");
+		if (M < 2) {
+			System.out.println("YES");
+			return;
 		}
+		
+		List<Node> list = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			list.add(new Node(i, Integer.parseInt(st.nextToken())));
+		}
+		Collections.sort(list, (a,b) -> Long.compare(a.weight, b.weight));
+		
+		boolean[][] check = new boolean[N][2];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int j = Integer.parseInt(st.nextToken())-1;
+			int k = Integer.parseInt(st.nextToken())-1;
+			if ((j == 0 && k == N-1) || (k == 0 && j == N-1)) {
+				check[0][0] = true;
+				check[N-1][1] = true;
+			}else if (j < k){
+				check[k][0] = true;
+				check[j][1] = true;
+			}else {
+				check[j][0] = true;
+				check[k][1] = true;
+			}
+		}
+		long ans = mst(list, check);
+		System.out.println(ans > K ? "NO" : "YES");
 	}
-	static long mst(int N, List<Node> list, boolean[][] check) {
+	static long mst(List<Node> list, boolean[][] check) {
 		boolean[] visited = new boolean[N];
 		int[] dx = {-1,1};
 		long totalDist = 0;
