5279fe07d105c622e8cca6aeb12a77f81abe62a7
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-11-03 23:57:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 76.91 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s)
+{   var answer = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        answer.push(s[i]);
+        if (answer[answer.length-1] === answer[answer.length-2]) {
+            answer.pop();
+            answer.pop();
+        }
+    }
+
+    return answer.length === 0 ? 1 : 0;
+}
