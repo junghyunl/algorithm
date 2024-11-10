8405475c82bf902b016103990ab21aaf0d2a913d
junghyunl <83959932+junghyunl@users.noreply.github.com>
2024-11-11 00:49:55 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 44.67 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -9,28 +9,27 @@ function solution(arrayA, arrayB) {
     let gcdA = arrayA[0];
     let gcdB = arrayB[0];
     for (let i = 1; i < N; i++) {
+        if (gcdA === 1) break;
         if (gcdA < arrayA[i]) gcdA = gcd(arrayA[i], gcdA);
         else gcdA = gcd(gcdA, arrayA[i]);
-            
+    }
+    for (let i = 1; i < N; i++) {
+        if (gcdB === 1) break;
         if (gcdB < arrayB[i]) gcdB = gcd(arrayB[i], gcdB);
         else gcdB = gcd(gcdB, arrayB[i]);
     }
     
-    let pos = [true, true];
     for (let i = 0; i < N; i++) {
         if (arrayA[i]%gcdB === 0) {
-            pos[0] = false;
+            gcdB = 0;
             break;
         }
     }
     for (let i = 0; i < N; i++) {
         if (arrayB[i]%gcdA === 0) {
-            pos[1] = false;
+            gcdA = 0;
             break;
         }
     }
-    if (pos[0] && pos[1]) return Math.max(gcdA, gcdB);
-    else if (!pos[0] && !pos[1]) return 0;
-    else if (pos[0]) return gcdB;
-    else return gcdA;
+    return Math.max(gcdA, gcdB);
 }
