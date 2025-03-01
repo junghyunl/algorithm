f85fb112afb37e8a15881478b3eb7499a435fca7
junghyunl <83959932+junghyunl@users.noreply.github.com>
2025-03-02 00:25:09 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 68.11 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -0,0 +1,43 @@
+function solution(cap, n, deliveries, pickups) {
+    let ans = 0, lastDeliverIdx = n-1, lastPickupIdx = n-1;
+    
+    while (deliveries[lastDeliverIdx] === 0 && lastDeliverIdx > -1) {
+        deliveries.pop();
+        lastDeliverIdx--;
+    }
+    
+    while (pickups[lastPickupIdx] === 0 && lastPickupIdx > -1) {
+        pickups.pop();
+        lastPickupIdx--;
+    }
+    
+    while (lastDeliverIdx > -1 || lastPickupIdx > -1) {
+        let deliver = cap, pickup = cap;
+        
+        ans += Math.max(lastDeliverIdx+1, lastPickupIdx+1)*2;
+        
+        while (deliver > 0 && lastDeliverIdx > -1) {
+            const cnt = Math.min(deliver, deliveries[lastDeliverIdx]);
+            deliveries[lastDeliverIdx] -= cnt;
+            deliver -= cnt;
+            
+            while (deliveries[lastDeliverIdx] === 0 && lastDeliverIdx > -1) {
+                deliveries.pop();
+                lastDeliverIdx--;
+            }
+        }
+        
+        while (pickup > 0 && lastPickupIdx > -1) {
+            const cnt = Math.min(pickup, pickups[lastPickupIdx]);
+            pickups[lastPickupIdx] -= cnt;
+            pickup -= cnt;
+            
+            while (pickups[lastPickupIdx] === 0 && lastPickupIdx > -1) {
+                pickups.pop();
+                lastPickupIdx--;
+            }
+        }
+    }
+    
+    return ans;
+}
